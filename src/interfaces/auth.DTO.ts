5560547273952bc5c0ceef17f3aff534591d8cee
Anarragan <anbarragana@unal.edu.co>
2025-10-22 09:57:25 -0500

    feat: Implementar modelo y servicio de autenticación

---
+++ src/interfaces/auth.DTO.ts
@@ -0,0 +1,13 @@
+export interface IRegisterDTO {
+    name?: string;
+    email: string;
+    password: string;
+    created_at?: Date;
+    updated_at?: Date;
+    role: "admin" | "seller";
+}
+
+export interface ILoginDTO {
+    email: string;
+    password: string;
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import { routerAuth } from "./auth.routes.js";
 const router = Router();
 
 router.get("/", (req, res) => {
     res.send("Welcome to the SportsLine API!");
 });
+
+router.use("/auth", routerAuth);
+
 
 export default router;
